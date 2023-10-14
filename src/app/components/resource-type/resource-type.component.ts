ed4c752e3ab334f2f6193cac5a75f70ca61ac070
mohamedhany01 <mohamedhyouns@gmail.com>
2023-10-14 18:47:36 +0300

    Optimize state 3

--- src/app/components/resource-type/resource-type.component.ts
+++ src/app/components/resource-type/resource-type.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
 import { BreakpointService } from 'src/app/services/breakpoint.service';
+import { addSettings } from 'src/app/store/actions/app.action';
+import { AppSettings } from 'src/app/store/modules/app-settings.interface';
+import { selectSettings } from 'src/app/store/selectors/app.selector';
 
 @Component({
   selector: 'app-resource-type',
@@ -10,5 +14,34 @@ import { BreakpointService } from 'src/app/services/breakpoint.service';
 export class ResourceTypeComponent {
   isMedium$: Observable<boolean> = this.breakpointObserver.isViewportMedium();
 
-  constructor(private breakpointObserver: BreakpointService) {}
+  appSettings$: Observable<AppSettings>;
+  appSettingsSubscription!: Subscription;
+
+  multiple: boolean | undefined;
+  once: boolean | undefined;
+
+  constructor(
+    private breakpointObserver: BreakpointService,
+    private store: Store
+  ) {
+    this.appSettings$ = this.store.select(selectSettings);
+
+    this.appSettings$.subscribe(settings => {
+      this.once = settings.resourceType?.oneDay;
+      this.multiple = settings.resourceType?.moreThanOneDay;
+    });
+  }
+
+  onChange() {
+    this.store.dispatch(
+      addSettings({
+        settings: {
+          resourceType: {
+            oneDay: this.once,
+            moreThanOneDay: this.multiple,
+          },
+        },
+      })
+    );
+  }
 }
